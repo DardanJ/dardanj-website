e4882342aed35c478cf9a024affac7cc0fa6960c
DardanJ <99636674+DardanJ@users.noreply.github.com>
2025-05-10 00:48:14 +1000

    📝style: increase navbar text size and add small intro to home page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,8 +4,8 @@ import { NavLink } from 'react-router-dom';
 export default function NavBar() {
   return (
     <nav className ="container mx-auto flex justify-between items-center px-6 py-4 border-b border-gray-700">
-      <Link to="/" className ="text-lg hover:text-sky-600"> 🏠 dardanj.com </Link>
-      <div className ="space-x-6 text-sm">
+      <Link to="/" className ="text-xl hover:text-sky-600"> 🏠 dardanj.com </Link>
+      <div className ="space-x-6 text-lg">
         <NavLink to ="/projects" className={({ isActive }) => isActive ? "text-sky-600" : "hover:text-sky-600"}> 🧑‍💻 Projects </NavLink>
         <NavLink to ="/Resume" className={({ isActive }) => isActive ? "text-sky-600" : "hover:text-sky-600"}> 📄 Resume </NavLink>
         <NavLink to ="/Contact" className={({ isActive }) => isActive ? "text-sky-600" : "hover:text-sky-600"}> 📅 Contact </NavLink>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,12 @@ export default function Home() {
     return (
       <main>
         <h1 class = "text-7xl text-center py-20 font-extrabold">Dardan Jerliu</h1>
-        <p></p>
+        <p className="text-center text-xl max-w-2xl mx-auto px-4">
+        Hello! I'm Dardan a software developer with a passion for full-stack web development,
+        database design and systems administration. I enjoy building scalable and
+        user-focused applications from designing data models to deploying backend services and 
+        crafting responsive UIs.
+        </p>
       </main>
     );
   }
